32e83b1f82b6c6fa01247c6cc1605194c724c307
Ali Haider <ali1500haider@gmail.com>
2024-03-23 22:44:06 +0500

    update Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,11 +11,11 @@ export function Map() {
         className="h-full w-full"
         center={position}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
           ``
